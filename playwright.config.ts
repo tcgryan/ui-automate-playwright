729190ae0fb4c2a5456ddf69129a095aec7daa80
tcgryan <87990652+tcgryan@users.noreply.github.com>
2024-05-18 20:21:54 -0400

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 import * as dotenv from "dotenv";
 
-const env = process.env.NODE_ENV ?? 'qa';
+const env = process.env.NODE_ENV;
 // const env = 'staging';
 dotenv.config({ path: `.env.${env}`});
 /**
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 0 : 3,
+  workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
